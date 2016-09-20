d30f283fa1b0e6a30e32b3f3e551b8ae85381459
Ted Barnett <mail@tedbarnett.com>
2016-09-20 11:44:10 -0700

    Initial

---
+++ js/audioAssets.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title': 'Question 65: Describe an error you made, why you made it, and the impact it had on your life.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935087.45316-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100065.wav'
+    },
+    {
+        'title': 'Question 79: Have you ever been a whistleblower, exposing something unethical or illegal? If so, describe it.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935287.18748-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100079.wav'
+    },
+    {
+        'title': 'Question 67: Name one of your favorite books, and explain why it’s a favorite.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935334.67451-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100067.wav'
+    },
+    {
+        'title': 'Question 73: Have you ever had an accident? Describe it, and what you learned from the experience.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935408.81362-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100073.wav'
+    },
+    {
+        'title': 'Question 65: Describe an error you made, why you made it, and the impact it had on your life.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935087.45316-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100065.wav'
+    },
+    {
+        'title': 'Question 79: Have you ever been a whistleblower, exposing something unethical or illegal? If so, describe it.',
+        'url': 'https://s3.amazonaws.com/boswellapp/1472935287.18748-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100079.wav'
+    }
+];
+
+module.exports = audioData;
