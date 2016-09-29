282e32bde9de40961449b55dff9895479d522767
Ted Barnett <mail@tedbarnett.com>
2016-09-29 14:14:19 -0700

    Fixed array.push

--- js/boswellStories.js
+++ js/boswellStories.js
@@ -36,17 +36,14 @@ docClient.get(params, function (err, data) {
     } else {
 
         var audioData = [];
-        audioData[1] =
-            {
-                'title': data.Item.transcription,
-                'url': data.Item.filename
-            };
-
-        audioData[2] =
-            {
-                'title': data.Item.transcription,
-                'url': data.Item.filename
-            };
+        audioData.push({
+            'title': data.Item.transcription,
+            'url': data.Item.filename
+        });
+        audioData.push({
+            'title': "2. " + data.Item.transcription,
+            'url': data.Item.filename
+        });
  //       console.log("data.Item.filename: ", data.Item.filename); // data is properly loaded from the "data" db call above
         console.log("audioData=", audioData); // displays the correct result when I run this on my console (and on Lambda)
 
@@ -74,26 +71,7 @@ docClient.get(params, function (err, data) {
 //    {
 //        'title': 'Question 79: Have you ever been a whistleblower, exposing something unethical or illegal? If so, describe it.',
 //        'url': 'https://s3.amazonaws.com/boswellapp/1472935287.18748-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100079.wav'
-//    },
-//    {
-//        'title': 'Question 67: Name one of your favorite books, and explain why it�s a favorite.',
-//        'url': 'https://s3.amazonaws.com/boswellapp/1472935334.67451-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100067.wav'
-//    },
-//    {
-//        'title': 'Question 73: Have you ever had an accident? Describe it, and what you learned from the experience.',
-//        'url': 'https://s3.amazonaws.com/boswellapp/1472935408.81362-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100073.wav'
-//    },
-//    {
-//        'title': 'Question 65: Describe an error you made, why you made it, and the impact it had on your life.',
-//        'url': 'https://s3.amazonaws.com/boswellapp/1472935087.45316-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100065.wav'
-//    },
-//    {
-//        'title': 'Question 79: Have you ever been a whistleblower, exposing something unethical or illegal? If so, describe it.',
-//        'url': 'https://s3.amazonaws.com/boswellapp/1472935287.18748-1967D471-70F6-4BD7-9C03-7FEFB75B3D5F-100079.wav'
 //    }
 //];
 
 //module.exports = audioData;
-
-
-
